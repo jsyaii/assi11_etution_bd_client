b0ae36378d88d7fc6562903e1016a7141584c3e8
jsyaii <jasiahasan1703@gmail.com>
2025-12-15 01:53:56 +0600

    routeer updatee

--- src/Routes/StudentRoute.jsx
+++ src/Routes/StudentRoute.jsx
@@ -1,14 +1,14 @@
-// import { Navigate } from "react-router";
-// import useRole from "../hook/useRole";
+import { Navigate } from "react-router";
+import useRole from "../hook/useRole";
 
 
-// const StudentRoute = ({ children }) => {
-//   const { role, roleLoading } = useRole();
+const StudentRoute = ({ children }) => {
+  const { role, roleLoading } = useRole();
 
-//   if (roleLoading) return <p>Checking role...</p>;
-//   if (role !== "student") return <Navigate to="/unauthorized" replace />;
+  if (roleLoading) return <p>Checking role...</p>;
+  if (role !== "student") return <Navigate to="/unauthorized" replace />;
 
-//   return children;
-// };
+  return children;
+};
 
-// export default StudentRoute;
+export default StudentRoute;
