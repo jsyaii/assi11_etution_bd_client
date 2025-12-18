79a86073018a3e86427ed7aed127146a1b853a43
jsyaii <jasiahasan1703@gmail.com>
2025-12-18 19:03:21 +0600

    loading

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,11 +1,11 @@
-// import '../../App.css';
+import '../../App.css';
 
-// const Loading = () => {
-//   return (
-//     <div className="flex flex-col items-center justify-center gap-5 min-h-[550px]">
-//       <div className="loader"></div>
-//     </div>
-//   );
-// };
+const Loading = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-5 min-h-[550px]">
+      <div className="loader"></div>
+    </div>
+  );
+};
 
-// export default Loading;
+export default Loading;
