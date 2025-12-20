f2ee535144378b677b541a1ad67861e83b9b6780
jsyaii <jasiahasan1703@gmail.com>
2025-12-20 23:16:47 +0600

    approved tuitions

--- src/pages/Tutor/ApprovedTuitions/ApprovedTuitions.jsx
+++ src/pages/Tutor/ApprovedTuitions/ApprovedTuitions.jsx
@@ -1,76 +1,76 @@
 
 
-// import { useNavigate } from "react-router";
-// import useAuth from "../../../hook/useAuth";
-// import useAxiosSecure from "../../../hook/useAxiosSecure";
-// import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
+import useAuth from "../../../hook/useAuth";
+import useAxiosSecure from "../../../hook/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
-// const ApprovedTuitions = () => {
-//     const { user } = useAuth();
-//     const axiosSecure = useAxiosSecure();
-//     const tutorEmail = user.email;
-//     const navigate = useNavigate();
+const ApprovedTuitions = () => {
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const tutorEmail = user.email;
+    const navigate = useNavigate();
 
-//     const {
-//         data: myApprovedApplications = [],
-//         refetch
-//     } =
-//         useQuery({
-//             queryKey: [`my-approved-applications`, tutorEmail],
-//             queryFn: async () => {
-//                 const res = await axiosSecure.get(`/applications/approved/${tutorEmail}`)
-//                 return res.data;
-//             }
-//         })
+    const {
+        data: myApprovedApplications = [],
+        refetch
+    } =
+        useQuery({
+            queryKey: [`my-approved-applications`, tutorEmail],
+            queryFn: async () => {
+                const res = await axiosSecure.get(`/applications/approved/${tutorEmail}`)
+                return res.data;
+            }
+        })
 
-//     return (
-//         <div className="overflow-x-auto">
-//             <table className="table">
-//                 <thead>
-//                     <tr className="">
-//                         <th>#</th>
-//                         <th className="text-center">Subject</th>
-//                         <th className="text-center">Status</th>
-//                         <th className="text-center">Salary</th>
-//                         <th className="text-center">Payment Time</th>
-//                     </tr>
-//                 </thead>
+    return (
+        <div className="overflow-x-auto">
+            <table className="table">
+                <thead>
+                    <tr className="">
+                        <th>#</th>
+                        <th className="text-center">Subject</th>
+                        <th className="text-center">Status</th>
+                        <th className="text-center">Salary</th>
+                        <th className="text-center">Payment Time</th>
+                    </tr>
+                </thead>
 
-//                 <tbody>
-//                     {myApprovedApplications.map((application, index) => (
-//                         <tr
-//                             key={application._id}
-//                             className="hover:bg-white/10 cursor-pointer transition"
-//                             onClick={() => navigate(`/tuitions/${application.tuitionId}`)}
-//                         >
-//                             <th>{index + 1}</th>
+                <tbody>
+                    {myApprovedApplications.map((application, index) => (
+                        <tr
+                            key={application._id}
+                            className="hover:bg-white/10 cursor-pointer transition"
+                            onClick={() => navigate(`/tuitions/${application.tuitionId}`)}
+                        >
+                            <th>{index + 1}</th>
 
-//                             <td className="text-center">{application.tuitionSubject}</td>
+                            <td className="text-center">{application.tuitionSubject}</td>
 
-//                             <td className={`text-center font-medium ${application.applicationStatus === `Approved` ? "text-green-500" : "text-amber-500"}`} >
-//                                 {application.applicationStatus === "Approved"
-//                                     ? "Approved"
-//                                     : "Pending Approval"}
-//                             </td>
+                            <td className={`text-center font-medium ${application.applicationStatus === `Approved` ? "text-green-500" : "text-amber-500"}`} >
+                                {application.applicationStatus === "Approved"
+                                    ? "Approved"
+                                    : "Pending Approval"}
+                            </td>
 
-//                             <td className="text-center">৳ {application.tutorSalary}</td>
-//                             <td className="text-center"> {application.paidAt ? new Date(application.paidAt).toLocaleDateString('en', {
-//                                 day: 'numeric',
-//                                 month: 'short',
-//                                 year: 'numeric',
-//                                 hour: '2-digit',
-//                                 minute: '2-digit',
-//                                 hour12: true
-//                             })
-//                                 : 'N/A'}
-//                             </td>
+                            <td className="text-center">৳ {application.tutorSalary}</td>
+                            <td className="text-center"> {application.paidAt ? new Date(application.paidAt).toLocaleDateString('en', {
+                                day: 'numeric',
+                                month: 'short',
+                                year: 'numeric',
+                                hour: '2-digit',
+                                minute: '2-digit',
+                                hour12: true
+                            })
+                                : 'N/A'}
+                            </td>
 
-//                         </tr>
-//                     ))}
-//                 </tbody>
-//             </table>
-//         </div>
-//     );
-// };
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
 
-// export default ApprovedTuitions;
+export default ApprovedTuitions;
